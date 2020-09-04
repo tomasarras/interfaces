0a70be5279a9c42b2bc3a16dc70226b1e65cdbe0
Tomas Arras <tomasarras@gmail.com>
2020-09-04 17:03:43 -0300

    Se agrego el filtro blanco y negro

--- Tp1/7/js/script.js
+++ Tp1/7/js/script.js
@@ -1,12 +1,52 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     let ctx = document.querySelector("#myCanvas").getContext('2d');
+    let btnBlancoNegro = document.querySelector("#js-filtro-blanco-negro");
+    btnBlancoNegro.addEventListener("click",agregarFiltroBlancoNegro);
 
     let imagen = new Image();
-    imagen.src = "./img/download.png";
+    imagen.src = "./img/download.jpg";
     imagen.onload = function() { dibujarImagen(this) };
 
     function dibujarImagen(imagen) {
         ctx.drawImage(imagen,0,0);
+    }
+
+    function agregarFiltroBlancoNegro() {
+        let imageData = ctx.getImageData(0,0,1024,512);
+        for (let x = 0; x < imageData.width; x++) {
+            for (let y = 0; y < imageData.height; y++) {
+                r = getRed(imageData,x,y);
+                g = getGreen(imageData,x,y);
+                b = getBlue(imageData,x,y);
+
+                prom = (r + g + b) / 3;
+                setPixel(imageData,x,y,prom,prom,prom,255);
+            }
+        }
+        ctx.putImageData(imageData,0,0);
+    }
+
+    function getRed(imageData,x,y){
+        index = (x + y * imageData.width) * 4;
+        return imageData.data[index+0];
+    }
+
+    function getGreen(imageData,x,y){
+        index = (x + y * imageData.width) * 4;
+        return imageData.data[index+1];
+    }
+
+    function getBlue(imageData,x,y){
+        index = (x + y * imageData.width) * 4;
+        return imageData.data[index+2];
+    }
+
+    function setPixel(imageData,x,y,r,g,b,a) {
+        let index = (x + y * imageData.width) * 4;
+        imageData.data[index+0] = r;
+        imageData.data[index+1] = g;
+        imageData.data[index+2] = b;
+        imageData.data[index+3] = a;
     }
 
 });
