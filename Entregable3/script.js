157091d3c0e7fb763dd4f970db927c2aba065864
Tomas Arras <tomasarras@gmail.com>
2020-10-11 14:50:56 -0300

    entregable 3 - portada terminada

--- Entregable3/script.js
+++ Entregable3/script.js
@@ -9,8 +9,8 @@ document.addEventListener("DOMContentLoaded",()=>{
     }
 
     function animarCarga() {
-        let loading = document.querySelector(".loading");
-        loading.classList.add("terminado");
+        let hero = document.querySelector(".hero");
+        hero.classList.add("terminado");
         setTimeout(() => {
             let smoke = document.querySelector(".smoke");
             smoke.classList.add("active");
@@ -20,7 +20,47 @@ document.addEventListener("DOMContentLoaded",()=>{
     }
     
     function cargarPagina() {
-        let loading = document.querySelector(".loading");
-        loading.classList.add("none");
+        let hero = document.querySelector(".hero");
+        hero.classList.add("none");
+        mostrarElementosPagina();
+        setTimeout(() => {
+            hero.classList.add("display-none");
+            let bttf = document.querySelector(".back-to-the-future");
+            let delorean = document.querySelector(".portada .delorean");
+            delorean.classList.add("active");
+            bttf.classList.add("active");
+        }, 1000);
+    }
+
+    function mostrarElementosPagina() {
+        let elementos = document.querySelectorAll(".display-none");
+        elementos.forEach(elemento => {
+            elemento.classList.remove("display-none");
+        });
+        console.log(elementos);
+    }
+
+    const viewHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    window.addEventListener("scroll", ()=>{
+        let coordenadaY = document.documentElement.scrollTop;
+        let delorean = document.querySelector(".portada .delorean");
+        console.log(delorean);
+        let normalizacion = coordenadaY / viewHeight;// numero entre 0 y 1
+        //0: inicio de la pagina, 1: cuando ya paso el hero
+        delorean.style.transform = "translateY(" + (normalizacion * 80) + "%)";
+        if (coordenadaY == 0)
+            mostrarIconoMouseWheel();
+        else
+            ocultarIconoMouseWheel();
+    });
+
+    function mostrarIconoMouseWheel() {
+        let icono = document.querySelector(".mouse-wheel");
+        icono.style.opacity = 1;
+    }
+
+    function ocultarIconoMouseWheel(){ 
+        let icono = document.querySelector(".mouse-wheel");
+        icono.style.opacity = 0;
     }
 });
