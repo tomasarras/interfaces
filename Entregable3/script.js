35f139b91ca8cce680ad608c0f652731159e45ec
Tomas Arras <tomasarras@gmail.com>
2020-10-12 19:48:01 -0300

    entregable 3 cards terminadas

--- Entregable3/script.js
+++ Entregable3/script.js
@@ -37,14 +37,12 @@ document.addEventListener("DOMContentLoaded",()=>{
         elementos.forEach(elemento => {
             elemento.classList.remove("display-none");
         });
-        console.log(elementos);
     }
 
     const viewHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
     window.addEventListener("scroll", ()=>{
         let coordenadaY = document.documentElement.scrollTop;
         let delorean = document.querySelector(".portada .delorean");
-        console.log(delorean);
         let normalizacion = coordenadaY / viewHeight;// numero entre 0 y 1
         //0: inicio de la pagina, 1: cuando ya paso el hero
         delorean.style.transform = "translateY(" + (normalizacion * 80) + "%)";
@@ -62,5 +60,72 @@ document.addEventListener("DOMContentLoaded",()=>{
     function ocultarIconoMouseWheel(){ 
         let icono = document.querySelector(".mouse-wheel");
         icono.style.opacity = 0;
+    }
+
+
+
+
+
+
+    const X = 0;
+    const Y = 1;
+    let divCards = document.querySelectorAll(".card");
+    divCards.forEach(divCard => {
+        let imgCard = divCard.children[0];
+        divCard.addEventListener("mousemove",(e) =>{
+            moverCard(e,imgCard);
+        });
+        divCard.addEventListener("mouseleave",() => restaurarPosicion(imgCard));
+    });
+
+    function moverCard(e,card) {
+        const limiteGrados = 30; //intensidad de rotacion de la card
+        const limiteSombra = 30; // desplazamiento de la sombra
+        card.classList.remove("restaurar-posicion");
+        let coordenadas = getPosicion(e,card);
+        let normalizarX = coordenadas[X] / card.offsetWidth;
+        let normalizarY = coordenadas[Y] / card.offsetHeight;
+        let anguloX,anguloY,sombraX,sombraY;
+
+        anguloX = (normalizarX / 0.5) * limiteGrados;
+        anguloX = limiteGrados - anguloX;
+        anguloX = anguloX *-1;
+
+        anguloY = (normalizarY / 0.5) * limiteGrados;
+        anguloY = limiteGrados - anguloY;
+
+        sombraX = (normalizarX / 0.5) * limiteSombra;
+        sombraX = limiteSombra - sombraX;
+        sombraX = sombraX;
+
+        sombraY = (normalizarY / 0.5) * limiteSombra;
+        sombraY = limiteGrados - sombraY;
+
+        let rotacion = "rotateX(" + anguloY + "deg) rotateY(" + anguloX + "deg)";
+        let sombra = sombraX+"px "+sombraY+"px "+"14px 6px rgba(0,0,0,0.75)";
+
+        card.style['-webkit-box-shadow'] = sombra;
+        card.style['-moz-box-shadow'] = sombra;
+        card.boxShadow = sombra;
+        card.style.transform = rotacion;
+        console.log(card.width)
+    }
+
+    function restaurarPosicion(card) {
+        card.classList.add("restaurar-posicion");
+        card.style.transform = "rotate(0)";
+        let sombra = "0 0 14px 6px rgba(0,0,0,0.75)";
+        card.style['-webkit-box-shadow'] = sombra;
+        card.style['-moz-box-shadow'] = sombra;
+        card.boxShadow = sombra;
+    }
+
+    function getPosicion(e,card) {
+        let coordenadas = new Array();
+        let x = e.pageX - card.offsetLeft;
+        let y = e.pageY - card.offsetTop;
+        coordenadas[X] = x;
+        coordenadas[Y] = y;
+        return coordenadas;
     }
 });
