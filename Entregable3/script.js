3127d8d02192fcba5bcf77d5fc4487e4aedb5c22
Tomas Arras <tomasarras@gmail.com>
2020-10-18 15:04:27 -0300

    countdown listo

--- Entregable3/script.js
+++ Entregable3/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
     const TIMPO_FORZADO = 3000;
     const DURACION_ANIMACION = 2500;
+    const FECHA_ESTRENO = new Date("10/28/2020/15:00");
     
     setTimeout(terminarAnimacion, TIMPO_FORZADO);
     function terminarAnimacion() {
@@ -50,6 +51,8 @@ document.addEventListener("DOMContentLoaded",()=>{
             mostrarIconoMouseWheel();
         else
             ocultarIconoMouseWheel();
+
+        console.log(normalizacion)
     });
 
     function mostrarIconoMouseWheel() {
@@ -61,11 +64,6 @@ document.addEventListener("DOMContentLoaded",()=>{
         let icono = document.querySelector(".mouse-wheel");
         icono.style.opacity = 0;
     }
-
-
-
-
-
 
     const X = 0;
     const Y = 1;
@@ -108,7 +106,6 @@ document.addEventListener("DOMContentLoaded",()=>{
         card.style['-moz-box-shadow'] = sombra;
         card.boxShadow = sombra;
         card.style.transform = rotacion;
-        console.log(card.width)
     }
 
     function restaurarPosicion(card) {
@@ -127,5 +124,53 @@ document.addEventListener("DOMContentLoaded",()=>{
         coordenadas[X] = x;
         coordenadas[Y] = y;
         return coordenadas;
+    }
+
+    setInterval(actualizarEstreno, 1000);
+    actualizarEstreno();
+    function actualizarEstreno() {
+        let tiempoRestante = getTiempoRestante();
+        document.querySelector(".display.sec").innerHTML = tiempoRestante.segundosRestantes;
+        document.querySelector(".display.min").innerHTML = tiempoRestante.minutosRestantes;
+        document.querySelector(".display.hour").innerHTML = tiempoRestante.horasRestantes;
+        document.querySelector(".display.day").innerHTML = tiempoRestante.diasRestantes;
+        document.querySelector(".display.month").innerHTML = tiempoRestante.mesesRestantes;
+        document.querySelector(".display.year").innerHTML = tiempoRestante.aniosRestantes;
+    }
+
+    function getTiempoRestante() {
+        let dateNow = new Date();
+        let tiempoRestanteSegundos = (FECHA_ESTRENO - dateNow) / 1000;
+        let segundosRestantes = ("0" + Math.floor(tiempoRestanteSegundos % 60)).slice(-2);
+        let minutosRestantes = ("0" + Math.floor(tiempoRestanteSegundos / 60 % 60)).slice(-2);
+        let horasRestantes = ("0" + Math.floor(tiempoRestanteSegundos / 3600 % 24)).slice(-2);
+        let diasRestantes = ("0" + Math.floor(tiempoRestanteSegundos / (3600 * 24) % 30)).slice(-2);
+        let mesesRestantes = ("0" + Math.floor(tiempoRestanteSegundos / (3600 * 24 * 30) % 12)).slice(-2);
+        let aniosRestantes = ("000" + Math.floor(tiempoRestanteSegundos / (3600 * 24 * 365))).slice(-4);
+        return {
+            segundosRestantes,
+            minutosRestantes,
+            horasRestantes,
+            diasRestantes,
+            mesesRestantes,
+            aniosRestantes
+        }
+    }
+
+    function getMes(numero) {
+        switch(numero) {
+            case 0: return "ENE";
+            case 1: return "FEB";
+            case 2: return "MAR";
+            case 3: return "ABR";
+            case 4: return "MAY";
+            case 5: return "JUN";
+            case 6: return "JUL";
+            case 7: return "AGO";
+            case 8: return "SEP";
+            case 9: return "OCT";
+            case 10: return "NOV";
+            case 11: return "DIC";
+        }
     }
 });
